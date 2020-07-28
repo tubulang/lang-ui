d6221bf0c5540db1b6700a0030732963c2a36143
tubulang <1053736438standard@gmail.com>
2020-07-28 19:26:33 +0800

    feat: change to docs

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,6 @@
 export default {
   files: './src/**/*.{md,markdown,mdx}', // 识别的文件后缀
-  dest: 'doc-site', // 打包出来的文件目录名
+  dest: 'docs', // 打包出来的文件目录名
   title: 'tubulang-ui', // 站点标题
   typescript: true, // 组件源文件是通过typescript开发，需要打开此选项
 }
